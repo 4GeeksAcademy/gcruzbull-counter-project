74f266aebc735aa231aa1798e53905b24c9215c1
gcruzbull <153864295+gcruzbull@users.noreply.github.com>
2025-05-12 22:46:12 +0000

    creación componente Card

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,11 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import { Card } from './components/Card';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
+    <Card/>
   </React.StrictMode>,
 )
