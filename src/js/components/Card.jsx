3a49db84c73a6feb61f531f719f1c9e61ab5310c
gcruzbull <153864295+gcruzbull@users.noreply.github.com>
2025-05-30 00:25:19 +0000

    agregar reloj al counter y aumentar tamaño

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -6,7 +6,7 @@ import React, { useState } from "react";
 export function Card({digit}){
     return (
         <div>
-            <button className="btn btn-dark">{digit}</button>
+            <button className="btn btn-dark card-size">{digit}</button>
         </div>     
     )
 
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card } from "./Card";
-// import index.css from "../../styles/index.css";
+// import index.css from "../styles/index.css";
 
 
 //include images into your bundle
@@ -12,17 +12,8 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = (props) => {
 	return (
 		<div>
-
-
-			<div>
-				<span><i className="bi bi-clock"></i></span>
-			</div>
-			{/* <div className="d-flex mt-5 justify-content-center align-content-center"> */}
-			{/* <div>
-				
-                <FontAwesomeIcon icon="fa-regular fa-clock" style={{color: "#ffffff",}} />
-            </div> */}
-			<div className="d-flex mt-5 justify-content-center align-content-center">
+			<div className="d-flex mt-5 justify-content-center align-content-center" id="counter">
+				<Card digit={<i className="fa-regular fa-clock"></i>}/>
 				<Card digit={props.hour2}/>
 				<Card digit={props.hour}/>
 				<Card digit={props.min2}/>
@@ -30,9 +21,7 @@ const Home = (props) => {
 				<Card digit={props.sec2}/>
 				<Card digit={props.sec}/>
 			</div>
-
-		</div>
-		
+		</div>		
 	);
 };
 
